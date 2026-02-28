f6bd7760aeb36e79c6fd981d4d765dd37d0e96fb
AI Interview Dev <dev@ai-interview-app.com>
2026-02-28 14:37:29 +0530

    first-commit

--- packages/frontend/src/services/api.ts
+++ packages/frontend/src/services/api.ts
@@ -10,11 +10,11 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response) {
-      // Server ne response diya lekin status code 2xx nahi hai
+      
       console.log("Data:", error.response.data);
       console.log("Status:", error.response.status);
     } else if (error.request) {
-      // Request send hui par koi response nahi mila (MOST LIKELY YOUR ISSUE)
+    
       console.log("Request info:", error.request);
     } else {
       console.log("Error Message:", error.message);
